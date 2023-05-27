074cac720bbc765d78bd63d6898fde7bc6e8f865
Christopher Meddings <c.m.meddings@gmail.com>
2023-05-27 19:07:08 +0000

    Add event listeners for document loading and button clicks

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,3 +1,25 @@
+// Wait for the DOM to finish loading before running the game
+// Get the buttion elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    //this in the for loop reperesents the varible button that has just been clicked.
+    // I.e. if we click the additon button, this will refer to this.
+    // Let .... or .... is a more modern way of writing a traditional for loop
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
@@ -31,5 +53,5 @@ function displayMultiplyQuestion() {
 }
 
 function displayDivisionQuestion() {
-    
+
 }
